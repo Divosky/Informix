1a739f96236ba3c690e00befcf60969fbceef792
Divosky <divoskyy@gmail.com>
2018-06-08 14:18:54 +0200

    Make a gallery

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,6 +1,47 @@
 <template>
-  <section class="container has-text-centered">
-    Galeria
+  <section class="content has-text-centered">
+    <div class="columns is-multiline">
+      <div class="column is-two-thirds-tablet is-half-desktop is-one-quarter-fullhd">
+        <figure class="image is-3by2">
+          <img src="https://unsplash.it/300/200/?random&pic=1" alt="">
+        </figure>
+      </div>
+      <div class="column is-two-thirds-tablet is-half-desktop is-one-quarter-fullhd">
+        <figure class="image is-3by2">
+          <img src="https://unsplash.it/300/200/?random&pic=1" alt="">
+        </figure>
+      </div>
+      <div class="column is-two-thirds-tablet is-half-desktop is-one-quarter-fullhd">
+        <figure class="image is-3by2">
+          <img src="https://unsplash.it/300/200/?random&pic=1" alt="">
+        </figure>
+      </div>
+      <div class="column is-two-thirds-tablet is-half-desktop is-one-quarter-fullhd">
+        <figure class="image is-3by2">
+          <img src="https://unsplash.it/300/200/?random&pic=1" alt="">
+        </figure>
+      </div>
+      <div class="column is-two-thirds-tablet is-half-desktop is-one-quarter-fullhd">
+        <figure class="image is-3by2">
+          <img src="https://unsplash.it/300/200/?random&pic=1" alt="">
+        </figure>
+      </div>
+      <div class="column is-two-thirds-tablet is-half-desktop is-one-quarter-fullhd">
+        <figure class="image is-3by2">
+          <img src="https://unsplash.it/300/200/?random&pic=1" alt="">
+        </figure>
+      </div>
+      <div class="column is-two-thirds-tablet is-half-desktop is-one-quarter-fullhd">
+        <figure class="image is-3by2">
+          <img src="https://unsplash.it/300/200/?random&pic=1" alt="">
+        </figure>
+      </div>
+      <div class="column is-two-thirds-tablet is-half-desktop is-one-quarter-fullhd">
+        <figure class="image is-3by2">
+          <img src="https://unsplash.it/300/200/?random&pic=1" alt="">
+        </figure>
+      </div>
+    </div>
   </section>
 </template>
 
