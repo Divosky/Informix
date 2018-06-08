18d5871a146dbb579a5bfd341a8001f641840e96
Divosky <divoskyy@gmail.com>
2018-06-08 23:24:57 +0200

    Add content to 'oNas' page

--- src/components/oNas.vue
+++ src/components/oNas.vue
@@ -1,7 +1,15 @@
 <template>
-  <section class="content has-text-centered">
-    O nas
-  </section>
+  <article class="container content">
+    <header class="page-title">
+      <h3 class="h1">O nas</h3>
+    </header>
+    <section>
+      <p class="has-text-justified">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere explicabo amet pariatur quisquam, quo in. Deleniti, ab voluptatibus reiciendis fugiat eaque rem neque voluptates debitis possimus, rerum libero. Reiciendis, consequatur.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos dolore voluptatum cupiditate, aperiam rem soluta molestias facilis odio saepe mollitia commodi similique tenetur at atque, dolorum sint iusto ab. Sed!
+      </p>
+    </section>
+  </article>
 </template>
 
 <script>
