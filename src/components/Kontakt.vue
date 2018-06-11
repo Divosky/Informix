886e1735ea438ec6fdb7b70e757d9bfec5ee1463
Divosky <divoskyy@gmail.com>
2018-06-11 15:05:00 +0200

    Add content to 'Kontakt' Page

--- src/components/Kontakt.vue
+++ src/components/Kontakt.vue
@@ -1,6 +1,19 @@
 <template>
-  <section class="content has-text-centered">
-    Kontakt
+  <section>
+    <article class="container content">
+      <header class="page-title">
+        <h3 class="h1">Kontakt</h3>
+      </header>
+      <section>
+        <address>
+          <strong>Adres:</strong>&nbsp;Końskie, ulica Warszawska 19, 26-200 Końskie <br>
+          <strong>Telefon:</strong>&nbsp;41 372 60 82 <br>
+          <strong>E-mail:</strong>&nbsp;<a href="mailto:informix@infor-mix.pl">informix@infor-mix.pl</a> <br>
+          <strong>Strona:</strong>&nbsp;<a href="http://infor-mix.pl">http://infor-mix.pl</a>
+        </address>
+        <p class="m-t-2">Szukasz gdzie się znajdujemy? Przejdź do zakładki <a href="/Znajdz-nas">Znajdź Nas</a></p>
+      </section>
+    </article>
   </section>
 </template>
 
