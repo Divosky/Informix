c134a0886827b541d4bc8640a0245c6fd71cf823
Divosky <divoskyy@gmail.com>
2018-06-17 18:04:29 +0200

    Change photos in the gallery to real ones

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -21,12 +21,22 @@ export default {
   data: function () {
     return {
       images: [
-        'https://unsplash.it/700/500/?random&pic=1',
-        'https://unsplash.it/700/500/?random&pic=2',
-        'https://unsplash.it/700/500/?random&pic=3',
-        'https://unsplash.it/700/500/?random&pic=4',
-        'https://unsplash.it/700/500/?random&pic=5',
-        'https://unsplash.it/700/500/?random&pic=6',
+        './img/gallery/1.jpg',
+        './img/gallery/2.jpg',
+        './img/gallery/3.jpg',
+        './img/gallery/4.jpg',
+        './img/gallery/5.jpg',
+        './img/gallery/6.jpg',
+        './img/gallery/7.jpg',
+        './img/gallery/8.jpg',
+        './img/gallery/9.jpg',
+        './img/gallery/10.jpg',
+        './img/gallery/11.jpg',
+        './img/gallery/12.jpg',
+        './img/gallery/13.jpg',
+        './img/gallery/14.jpg',
+        './img/gallery/15.jpg',
+        './img/gallery/16.jpg',
       ],
       index: null
     };
