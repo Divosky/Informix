ed31df39131ef6d27211dc3ccf88c3083dff792f
Divosky <divoskyy@gmail.com>
2018-06-17 22:01:35 +0200

    Add content to 'PogotowieKomputerowe' page

--- src/components/PogotowieKomputerowe.vue
+++ src/components/PogotowieKomputerowe.vue
@@ -1,6 +1,16 @@
 <template>
-  <section class="content has-text-centered">
-    Pogotowie Komputerowe
+  <section>
+    <article class="container content">
+      <header class="page-title">
+        <h3 class="h1">Pogotowie Komputerowe</h3>
+      </header>
+      <section>
+        <div class="text-center">
+          <img :src="'./img/pogotowie.jpg'" alt>
+        </div>
+        <p class="m-t-2">Jeśli masz zlecenie, które wymaga tego, abyśmy do Ciebie przyjechali - wystarczy że wezwiesz nasze pogotowie komputerowe! Nasi specjaliści wsiądą w nasz firmowy pojazd i pojawią się u ciebie z pomocą. Wystarcz telefon, który znajdziesz na stronie kontaktowej.</p>
+      </section>
+    </article>
   </section>
 </template>
 
@@ -11,4 +21,10 @@ export default {
 </script>
 
 <style scoped>
+img {
+  max-height: 250px;
+}
+.text-center {
+  text-align: center;
+}
 </style>
