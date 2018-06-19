d331e9a78aef882c788b51937fdc5e268d125c5b
Divosky <divoskyy@gmail.com>
2018-06-19 14:29:53 +0200

    Fix typos in the offer

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,29 +7,27 @@
       <section>
         <ul>
           <li>kompleksowa obsługa firm</li>
-          <li>kasy fiskalne i drukarki fiskalne (wiodacy producenci: Novitus, elzab, emar, datecs, ipos) posiadamy uprawnienia na bileterki Emar</li>
-          <li>oprogramowanie ERP CRM, sprzedażowe-magazynowe (asseco wapro, insert GT i nexo, insoft PC market, simplex small business) instalacje, a wdrożenia, szkolenia, serwis</li>
-          <li>sieci teleinformatyczne projektowanie montaż konfiguracja serwis (szafy, switche, routery, sieci WiFi) sprzęt wiodących producentów</li>
-          <li>dla wymagających firm możliwość podpisania preferencyjnej umowy serwisowej (dodatkowe godziny pomocy technicznej telefonicznej lub wizyty serwisanta, sprzęt zastępczy, pierwszeństwo w przyjazdach serwisu do klienta) </li>
-          <li>monitoring wizyjny (kamery hdcvi. IP. Ahd. Łączność przewodowa i bezprzewodowa) projektowanie. Montaż. Konfiguracja. Serwis. Opieka
-</li>
+          <li>kasy fiskalne i drukarki fiskalne (wiodacy producenci: Novitus, Elzab, Emar, Datecs, Ipos), posiadamy uprawnienia na bileterki Emar</li>
+          <li>oprogramowanie ERP CRM, sprzedażowe-magazynowe (Asseco Wapro, Insert GT i Nexo, Insoft PC Market, Simplex Small Business) instalacje, wdrożenia, szkolenia, serwis</li>
+          <li>sieci teleinformatyczne, projektowanie, montaż, konfiguracja, serwis (szafy, switche, routery, sieci WiFi), sprzęt wiodących producentów</li>
+          <li>dla wymagających firm możliwość podpisania preferencyjnej umowy serwisowej (dodatkowe godziny pomocy technicznej telefonicznej lub wizyty serwisanta, sprzęt zastępczy, pierwszeństwo w przyjazdach serwisu do klienta)</li>
+          <li>monitoring wizyjny (kamery HDCVI, IP, AHD, łączność przewodowa i bezprzewodowa), projektowanie, montaż, konfiguracja, serwis, opieka</li>
           <li>terminale płatnicze</li>
-          <li>sprzęt komputerowy: komputery (markowe Dell. Hp. Lenovo. Asus pro. Apple. Profesjonalnie składane zestawy pod zamówienie klienta. Na potrzeby stacja roboczych CAD cam, grafiki komputerowej, bezpieczeństwa danych). Peryferia (drukarki. Urządzenia wielofunkcyjne. Klawiatury. Myszy. Akcesoria ładowarki kable. Monitory. Duży wybór sprzetu posiadamy na miejscu w sklepie. Na zamówienie sprowadzamy dowolny sprzęt dla klienta na ogół w ciągu 24-48 godzin.</li>
-          <li> materiały eksploatacyjne do drukarek atramentowych, laserowych, iglowych. Oryginalne oraz zamienniki.</li>
-          <li>Kazdy towar na życzenie klienta dostarczymy na miejsce własnym środkiem transportu. Na terenie powiatu koneckiego bezpłatnie. 
-Świadczymy usługi serwisu u klienta w firmie lub w domu</li>
-          <li>montaż anten Lte. Gwarancja uzyskania lepszej prędkości internetu </li>
-          <li>kopiowanie kaset VHS oraz minidv na nośnik dvd lub pamięć przenosna</li>
-          <li>wagi sklepowe (zwykle kalkulacyjne wag-skanery) wiodacy producenci (cas dibal bizerba). Czytniki kodów kreskowych (wszystkie interfejsy: USB. KB. Bt. Rs232) szuflady kasowe (prowadzimy również serwis szuflad) </li>
-          <li>sprzęt komputerowy poleasingowy (notebooki Dell, komputery Dell hp, monitory Dell) tańsza alternatywa</li>
-          <li>papier A4 (niższa cena przy większych zakupach) papier komputerowy (składanka) rolki do kas i drukarek fiskalnych </li>
+          <li>sprzęt komputerowy: komputery (markowe Dell, Hp, Lenovo, Asus pro, Apple, profesjonalnie składane zestawy pod zamówienie klienta, na potrzeby stacja robocza CAD Cam, grafiki komputerowej, bezpieczeństwa danych). Peryferia (drukarki, urządzenia wielofunkcyjne, klawiatury, myszy, akcesoria, ładowarki, kable, monitory, duży wybór sprzetu posiadamy na miejscu w sklepie. Na zamówienie sprowadzamy dowolny sprzęt dla klienta, na ogół w ciągu 24-48 godzin.</li>
+          <li>materiały eksploatacyjne do drukarek atramentowych, laserowych, iglowych. Oryginalne oraz zamienniki.</li>
+          <li>Każdy towar na życzenie klienta dostarczymy na miejsce własnym środkiem transportu. Na terenie powiatu koneckiego bezpłatnie.</li>
+          <li>Świadczymy usługi serwisu u klienta w firmie lub w domu</li>
+          <li>montaż anten LTE. Gwarancja uzyskania lepszej prędkości internetu</li>
+          <li>kopiowanie kaset VHS oraz MiniDV na nośnik DVD lub pamięć przenosną</li>
+          <li>wagi sklepowe (zwykłe kalkulacyjne wag-skanery) wiodący producenci (Cas, Dibal, Bizerba). Czytniki kodów kreskowych (wszystkie interfejsy: USB, KB, BT, RS232), szuflady kasowe (prowadzimy również serwis szuflad)</li>
+          <li>sprzęt komputerowy poleasingowy (notebooki Dell, komputery Dell HP, monitory Dell) - tańsza alternatywa</li>
+          <li>papier A4 (niższa cena przy większych zakupach), papier komputerowy (składanka), rolki do kas i drukarek fiskalnych</li>
         </ul>
         <h3 class="m-t-2">Dodatkowe usługi:</h3>
         <ul>
           <li>punkt opłat Monetia</li>
           <li>punkt przyjęć i odbiorów paczek Inpost</li>
-          <li>punkt przyjęć i odbiorów awizowanych paczek gls (parcele shop)
-</li>
+          <li>punkt przyjęć i odbiorów awizowanych paczek GLS (Parcele Shop)</li>
           <li>kolorowe ksero A3</li>
           <li>wydruki a3, laminowanie, bindowanie</li>
         </ul>
